2fcd3a5f62b47c96b0870209a42c595348035856
GMoran5 <gumoran5@gmail.com>
2022-10-04 17:51:00 -0300

    Actualizacion

--- JS/codigo.js
+++ JS/codigo.js
@@ -1,18 +1,39 @@
-let producto = prompt("Ingrese el producto que desea comprar (Escriba fin para finalizar)");
-while(producto != "fin"){
+let productos = prompt("Ingrese el producto que desea comprar (Escriba fin para finalizar)");
+while(productos != "fin"){
 
-if ((producto == "Aro Margarita Grande") || (producto == "aro margarita grande")){
+if ((productos == "Aro Margarita Grande") || (productos == "aro margarita grande")){
     console.log("El precio del Aro Margarita Grande por unidad es $600");
-}else if ((producto == "Prendedor Colibri") || (producto =="prendedor colibri")){
+}else if ((productos == "Prendedor Colibri") || (productos =="prendedor colibri")){
     console.log("El precio del Prendedor Colibri es $400");
-}else if ((producto == "Atrapasueños 30cm") || (producto =="atrapasueños 30cm")){
+}else if ((productos == "Atrapasueños 30cm") || (productos =="atrapasueños 30cm")){
     console.log("El precio del Atrapasueños 30cm es $1500");
-}else if ((producto == "Llavero Mandala") || (producto =="llavero mandala")){
+}else if ((productos == "Llavero Mandala") || (productos =="llavero mandala")){
     console.log("El precio del Llavero Mandala es $500");
-}else if ((producto == "Colgante Frase") || (producto =="colgante frase")){
+}else if ((productos == "Colgante Frase") || (productos =="colgante frase")){
     console.log("El precio del Colgante Frase es $550");
 }else{
     alert("Producto sin stock!");
 }
-producto = prompt("Ingrese el producto que desea comprar (Escriba fin para finalizar");
+productos = prompt("Ingrese el producto que desea comprar (Escriba fin para finalizar");
 }
+
+function saludar(){
+    alert("Gracias por su compra!!!")
+}
+saludar();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
